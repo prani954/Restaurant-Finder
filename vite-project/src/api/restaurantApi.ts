fee2bf798d4accfdc8bfc2dc886fae0ffece79ec
Bhanu Pranaswi Sai <114332928+prani954@users.noreply.github.com>
2024-08-26 18:06:49 +0530

    Update restaurantApi.ts

--- vite-project/src/api/restaurantApi.ts
+++ vite-project/src/api/restaurantApi.ts
@@ -23,7 +23,8 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+// const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = '/api';
 
 export const getRestaurants = async () => {
     try {
